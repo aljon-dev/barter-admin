588ee835fa9553e4df2f39d897119cb3a9ceb97e
Meow_Dev <aljonjimenez091200@gmail.com>
2025-11-06 10:00:49 +0800

    updated

--- src/lib/AlertDialogs/AdminUserList/DeleteUser.svelte
+++ src/lib/AlertDialogs/AdminUserList/DeleteUser.svelte
@@ -1,25 +1,21 @@
 <script lang="ts">
- import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js";
+  import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js";
 
-
- 
+  let { isOpen = $bindable() } = $props();
 </script>
 
-
-<AlertDialog.Root>
- <AlertDialog.Trigger>Delete</AlertDialog.Trigger>
- <AlertDialog.Content>
-  <AlertDialog.Header>
-   <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
-   <AlertDialog.Description>
-    This action cannot be undone. This will permanently delete your account
-    and remove your data from our servers.
-   </AlertDialog.Description>
-  </AlertDialog.Header>
-  <AlertDialog.Footer>
-   <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-   <AlertDialog.Action>Continue</AlertDialog.Action>
-  </AlertDialog.Footer>
- </AlertDialog.Content>
+<AlertDialog.Root bind:open={isOpen}>
+  <AlertDialog.Content>
+    <AlertDialog.Header>
+      <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+      <AlertDialog.Description>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </AlertDialog.Description>
+    </AlertDialog.Header>
+    <AlertDialog.Footer>
+      <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+      <AlertDialog.Action>Continue</AlertDialog.Action>
+    </AlertDialog.Footer>
+  </AlertDialog.Content>
 </AlertDialog.Root>
-
--- src/routes/admin/Users/+page.svelte
+++ src/routes/admin/Users/+page.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
+  import * as Table from "$lib/components/ui/table/index.js";
+  import * as Menubar from "$lib/components/ui/menubar/index.js";
+  import DeleteUser from "$lib/AlertDialogs/AdminUserList/DeleteUser.svelte";
 
-    import * as Table from "$lib/components/ui/table/index.js";
-    import * as Menubar from "$lib/components/ui/menubar/index.js";
-	import DeleteUser from "$lib/AlertDialogs/AdminUserList/DeleteUser.svelte";
-
-
-    let {data} = $props();
-
-
+  let { data } = $props();
+  
+  let isOpenDelete = $state(false);
 </script>
-
-
 
 <div class='w-full px-6 py-8'>
   <div class="max-w-7xl mx-auto">
@@ -20,69 +16,63 @@
       <p class="text-muted-foreground mt-1">List of Users</p>
     </div>
 
-
-      <div>
-    <Table.Root>
-    <Table.Caption>A list of your recent Repots</Table.Caption>
-    <Table.Header>
-    <Table.Row>
-    <Table.Head >Id</Table.Head>
-    <Table.Head>Profile</Table.Head>
-    <Table.Head>Fullname</Table.Head>
-    <Table.Head>Ratings</Table.Head>
-    <Table.Head>Bio</Table.Head>
-    <Table.Head>Action</Table.Head>
-    </Table.Row>
-    </Table.Header>
-    <Table.Body>
-      {#each data.Users ?? [] as user}
-    <Table.Row>
-
-    <Table.Cell class="font-medium">{user.id}</Table.Cell>
-   <Table.Cell>
-    {#if user.profile === ''}
-        <div class="w-10 h-10 flex items-center justify-center bg-gray-300 rounded-full font-bold">
-        {user.firstname[0]}{user.lastname[0]}
-        </div>
-    {:else}
-        <img src={user.profile} alt="Profile" class="w-10 h-10 rounded-full object-cover" />
-    {/if}
-    </Table.Cell>
-    <Table.Cell>{user.firstname}  {user.lastname}</Table.Cell>
-    <Table.Cell> {#if user.totalrating && user.totalrating !== 0}
-        {(user.rating / user.totalrating).toFixed(1)}
-        {:else}
-             0
-        {/if}</Table.Cell>
-    <Table.Cell >{user.bio ?? 'No Bio'}</Table.Cell>
-    <Table.Cell> <script lang="ts">
- import * as Menubar from "$lib/components/ui/menubar/index.js";
-</script>
- 
-<Menubar.Root class='w-[100px]'>
- <Menubar.Menu>
-  <Menubar.Trigger>Account</Menubar.Trigger>
-  <Menubar.Content>
-   <Menubar.Item>
-    Edit
-   
-   </Menubar.Item>
-      <Menubar.Separator />
-   <Menubar.Item><DeleteUser/></Menubar.Item>
-   <Menubar.Separator />
-   <Menubar.Item>Banned</Menubar.Item>
-   <Menubar.Separator />
-
-  </Menubar.Content>
- </Menubar.Menu>
-</Menubar.Root> </Table.Cell>
-    </Table.Row>
-    {/each}
-    </Table.Body>
-    </Table.Root>
-
-        </div>   
-
-
+    <div>
+      <Table.Root>
+        <Table.Caption>A list of your recent Reports</Table.Caption>
+        <Table.Header>
+          <Table.Row>
+            <Table.Head>Id</Table.Head>
+            <Table.Head>Profile</Table.Head>
+            <Table.Head>Fullname</Table.Head>
+            <Table.Head>Ratings</Table.Head>
+            <Table.Head>Bio</Table.Head>
+            <Table.Head>Action</Table.Head>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {#each data.Users ?? [] as user}
+            <Table.Row>
+              <Table.Cell class="font-medium">{user.id}</Table.Cell>
+              <Table.Cell>
+                {#if user.profile === ''}
+                  <div class="w-10 h-10 flex items-center justify-center bg-gray-300 rounded-full font-bold">
+                    {user.firstname[0]}{user.lastname[0]}
+                  </div>
+                {:else}
+                  <img src={user.profile} alt="Profile" class="w-10 h-10 rounded-full object-cover" />
+                {/if}
+              </Table.Cell>
+              <Table.Cell>{user.firstname} {user.lastname}</Table.Cell>
+              <Table.Cell>
+                {#if user.totalrating && user.totalrating !== 0}
+                  {(user.rating / user.totalrating).toFixed(1)}
+                {:else}
+                  0
+                {/if}
+              </Table.Cell>
+              <Table.Cell>{user.bio ?? 'No Bio'}</Table.Cell>
+              <Table.Cell>
+                <Menubar.Root class='w-[100px]'>
+                  <Menubar.Menu>
+                    <Menubar.Trigger>Account</Menubar.Trigger>
+                    <Menubar.Content>
+                      <Menubar.Item>Edit</Menubar.Item>
+                      <Menubar.Separator />
+                      <Menubar.Item onclick={() => isOpenDelete = true}>Delete</Menubar.Item>
+                      <Menubar.Separator />
+                      <Menubar.Item>Banned</Menubar.Item>
+                    </Menubar.Content>
+                  </Menubar.Menu>
+                </Menubar.Root>
+              </Table.Cell>
+            </Table.Row>
+          {/each}
+        </Table.Body>
+      </Table.Root>
     </div>
+  </div>
+  
+  {#if isOpenDelete}
+    <DeleteUser bind:isOpen={isOpenDelete} />
+  {/if}
 </div>
